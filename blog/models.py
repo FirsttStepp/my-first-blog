a00e37851fcdda58042e718b8b912075d36fb8e3
HIMERA <PTNPNH722@gmail.com>
2021-07-26 14:20:18 +0300

    New update

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-    def publish(self):
+    def __str__publish(self):
         self.published_date = timezone.now()
         self.save()
 
